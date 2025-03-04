67a3dc59cc1a9f1f5e91ac0d7e9442fc10491c03
mervesekerfilizoglu <sekermer33@gmail.com>
2025-03-04 15:19:22 +0300

    bugs fixed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,30 +12,31 @@ public class Main {
         System.out.println(area(15,20));
         System.out.println(area(20));
     }
+
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-
-
+        // Saat geçersizse (0'dan küçük ya da 23'ten büyükse)
         if (clock < 0 || clock > 23) {
             return false;
         }
 
-        if (isBarking == true && (clock < 20 && clock > 8)) {
+        // Köpek havlıyorsa ve saat 8 akşam ile 8 sabah arasındaysa
+        if (isBarking && (clock >= 0 && clock < 8 || clock >= 20 && clock <= 23)) {
             return true;
-        } else {
-            return false;
         }
 
-
+        return false; // Diğer durumlarda false döneriz
     }
 
+
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-        if((firstAge<19 && firstAge>13)||(secondAge<19 && secondAge>13)||(thirdAge<19 && thirdAge>13))
+        if((firstAge>=13 && firstAge<=19)||(secondAge>=13 && secondAge<=19)||(thirdAge>=13 && thirdAge<=19))
         {
             return true;
         }
 
         return false;
     }
+
 
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
